f1762f31e17e2f833be5edc661047094d2115fbe
Anna Fry <annafry@students.berkeley.net>
2020-02-28 13:57:22 -0800

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,30 @@
+public class Player {
+
+    protected Board b = new Board();
+    protected String name;
+    protected String token;
+
+
+    public Player(String name, String token) {
+        this.name = name;
+        this.token = token;
+    }
+
+    //display board with nice text intro
+    public void display() {
+        System.out.println(name + "'s board:");
+        b.printBoard();
+    }
+
+    //win check routine with basic "algorithm" for now
+    //any move at 0,0 ends game
+    public boolean checkWin() {
+
+        if(b.grid[0][0] != "0") {
+            return true;
+        }
+
+        return false;
+
+    }
+}
